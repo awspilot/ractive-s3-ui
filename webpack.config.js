9f58375a4b2a29217a4a2b12ba51940ad6c5eefa
adrianpraja <adrian@databank.ro>
2020-02-15 12:32:51 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	node: false,
@@ -16,6 +17,7 @@ module.exports = {
 		})]
 	},
 	plugins: [
+		new MiniCssExtractPlugin({ filename: "[name].css" }), // { filename: "[name].[contentHash].css" }
 	],
 	entry: {
 		'ractive-s3-ui': path.resolve(__dirname, './src/index.js'),
@@ -41,6 +43,24 @@ module.exports = {
 	},
 	module: {
 		rules: [
+
+			{
+				test: /\.less$/,
+				use: [
+					MiniCssExtractPlugin.loader, // extract css into files
+					{
+						loader: 'css-loader', // translates CSS into CommonJS
+					},
+					{
+						loader: 'less-loader', // compiles Less to CSS
+						// options: {
+						//	paths: [path.resolve(__dirname, 'node_modules')],
+						// 	strictMath: true,
+						// 	noIeCompat: true,
+						// },
+					},
+				],
+			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
