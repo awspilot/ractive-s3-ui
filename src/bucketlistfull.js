43c950c10b283a184ebb2c698c86173c7a5d77a6
adrianpraja <adrian@databank.ro>
2020-02-16 12:47:21 +0200

    update

--- src/bucketlistfull.js
+++ src/bucketlistfull.js
@@ -16,7 +16,7 @@ export default Ractive.extend({
 			<a class='btn btn-xs btn-default {{#if refresh_tables }}disabled{{/if}}' on-click='@this.refresh_buckets()'> <icon-refresh /> </a>
 		</div>
 
-		<tabledata columns={{columns}} rows={{rows}} style='top: 38px;margin: 3px;' />
+		<tabledata columns={{columns}} rows={{rows}} on-colclick='open-item' style='top: 38px;margin: 3px;' />
 
 	`,
 	refresh_buckets: function() {
@@ -29,14 +29,15 @@ export default Ractive.extend({
 				return ractive.set('err', err )
 
 
-			//ractive.set({buckets: data.Buckets })
+			//console.log(data.Buckets)
+
 			ractive.set('rows', data.Buckets.map(function(b) {
 				return [
 					{ KEY: true },
-					{ S: b.Name },
-					{ },
-					{ },
-					{ }
+					{ HASH: b.Name, item: b, },
+					{ }, // access
+					{ }, // region
+					{ S: b.CreationDate, }  // date created
 
 
 
@@ -55,14 +56,99 @@ export default Ractive.extend({
 							return cb()
 
 						ractive.set('rows', ractive.get('rows').map(function(row) {
-							if ( row[1].S === b.Name ) {
+							if ( row[1].HASH === b.Name ) {
 								row[3].S = data.LocationConstraint || 'us-east-1'
 							}
 							return row
 						}))
 
-						cb()
+
+						// 'request.options' => array(
+						// 'proxy' => 'username:password@127.0.0.1:123' // socks5://127.0.0.1
+						// )
+
+
+						var this_s3 = new AWS.S3({
+							//endpoint: this.get('endpoint') || undefined,
+							//endpoint: 'http://localhost:8080/' + b.Name,
+
+							region: data.LocationConstraint || 'us-east-1',
+							credentials: {
+								accessKeyId: ractive.get('accessKeyId'),
+								secretAccessKey: ractive.get('secretAccessKey'),
+							},
+
+							//sslEnabled: false,
+
+
+							// to check
+							//s3BucketEndpoint: b.Name + '.s3.amazonaws.com',
+
+							// s3UseArnRegion default to true
+							// endpointDiscoveryEnabled (Boolean) — whether to enable endpoint discovery for operations that allow optionally using an endpoint returned by the service. Defaults to 'false'
+
+
+
+
+							//s3ForcePathStyle: false, // will add /?<REGION> but then signature will no longer match
+							// whether to marshal request parameters to the prefix of hostname. Defaults to true.
+							//hostPrefixEnabled: false,
+
+
+							// v2: SignatureDoesNotMatch, The request signature we calculated does not match the signature you provided. Check your key and signing method.
+							// v2: some regions do not support it
+
+							// v3: uses host in signature and will fail with AccessDenied
+
+							// v4: uses host in signature and will fail wit SignatureDoesNotMatch
+							//signatureVersion: 'v4',
+
+						});
+
+						var params = {
+							Bucket: b.Name,
+						};
+						this_s3.getBucketAcl(params, function(err, data) {
+							if (err)
+								return console.log(err)
+
+							var is_public_read = data.Grants.map(function(g) {
+								return { type: g.Grantee.Type, URI: g.Grantee.URI, perm: g.Permission, }
+								return
+									(g.Grantee.Type === 'Group')
+									//&& (g.Grantee.URI === "http://acs.amazonaws.com/groups/global/AllUsers" )
+									//&& ( g.Grantee.Permission === 'READ' )
+									;
+							}).filter(function(g) {
+								return (g.type === 'Group') && (g.URI === "http://acs.amazonaws.com/groups/global/AllUsers" ) && (g.perm === 'READ');
+							}).length > 0;
+
+							if (is_public_read) {
+								ractive.set('rows', ractive.get('rows').map(function(row) {
+									if ( row[1].HASH === b.Name ) {
+										row[2].S = 'PUBLIC'
+									}
+									return row
+								}))
+							}
+
+							cb()
+						});
+
+
+
+
+
+
+
+
 					})
+
+
+
+
+
+
 
 				}
 				return f;
@@ -98,7 +184,10 @@ export default Ractive.extend({
 		create() {
 			this.root.findComponent('tabs').newtab('bucketcreate', 'Create Bucket' )
 		},
-
+		'open-item': function( e, col, item, rawitem ) {
+			console.log('open-item', item.Name  )
+			this.root.findComponent('tabs').newtab('buckettab', item.Name )
+		},
 
 	}
 })
